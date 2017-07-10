5db7cd95c16c1b6b80c24540ca5ace4858e4d73b
Brian <briankimx91@gmail.com>
2017-07-10 11:42:14 -0700

    import intro example

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
-console.log('App working: open app/index.js to start exploring ES6');
+// console.log('App working: open app/index.js to start exploring ES6');
+import * as math from './math';
+import $ from 'jquery';
+
+const {num1, num2} = math;
+
+console.log(`add ${num1} and ${num2}, you will get ${math.add(num1,num2)}`);
+console.log(`subtract ${num1} and ${num2}, you will get ${math.subtract(num1,num2)}`);
+console.log(`divide ${num1} and ${num2}, you will get ${math.divide(num1,num2)}`);
+console.log(`multiply ${num1} and ${num2}, you will get ${math.multiply(num1,num2)}`);
+console.log(math);
+
+$('#root').append('<h1>I am Brian</h1>');
